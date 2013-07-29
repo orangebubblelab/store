29432b6ed21cd61ff147acb30fa829f56a26a88d
Billy Cheng <billy.cheng@orangebubblelab.com>
2013-07-29 15:26:28 +0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Macabolic::Application.config.secret_token = 'e89e009c573cd4391673edc0defd917ed42afd4f713f3713b5b49db353cb36e0a06ddb87350d31072b8cc25f2a8ee6e1ca0169f99adf61070a117e7d2ba484fc'
